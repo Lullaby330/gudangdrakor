e2dcba99e8f13d30f3df198a214be6055e4fcfb9
Lullaby330 <96850632+Lullaby330@users.noreply.github.com>
2022-04-26 18:41:05 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -11,7 +11,9 @@ from pyrogram.errors import FloodWait
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
-    if not FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL1:
+        return True
+    if not FORCE_SUB_CHANNEL2:
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
